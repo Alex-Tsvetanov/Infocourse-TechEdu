a8a288fd4e0bc63c057e546edd00c14b679bbc73
Alex Tsvetanov <alex.tsvetanov.2002@gmail.com>
2018-07-03 01:01:09 +0300

    add /trainings

---
+++ src/main/java/techedu/judge/controllers/TrainingController.java
@@ -0,0 +1,62 @@
+package techedu.judge.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import techedu.judge.entities.Course;
+import techedu.judge.entities.Track;
+import techedu.judge.repositories.base.GenericRepository;
+
+import java.util.List;
+
+@RequestMapping("/trainings")
+@Controller
+public class TrainingController {
+
+	@Autowired
+	GenericRepository<Track> trackRepo;
+
+	@Autowired
+	GenericRepository<Course> courseRepo;
+
+	@GetMapping (value = {"/tracks"})
+	public String showTracks (Model model)
+	{
+		model = techedu.judge.controllers.BaseController.setGuest (model);
+		List<Track> currentTrack = trackRepo.getAll ();
+		model = techedu.judge.controllers.BaseController.setGuest (model);
+
+		if (currentTrack.size () == 0) {
+			//model.addAttribute ("url", "/track/" + String.valueOf (id));
+			return "404";
+		}
+
+		model.addAttribute ("track.name", currentTrack.get (0).getName ());
+		model.addAttribute ("track.prefix", "/course");
+		model.addAttribute ("courses", currentTrack);
+
+		return "training";
+	}
+
+	@GetMapping (value = {"/courses"})
+	public String showCourses (Model model)
+	{
+		model = techedu.judge.controllers.BaseController.setGuest (model);
+		List<Course> currentTrack = courseRepo.getAll ();
+		model = techedu.judge.controllers.BaseController.setGuest (model);
+
+		if (currentTrack.size () == 0) {
+			//model.addAttribute ("url", "/track/" + String.valueOf (id));
+			return "404";
+		}
+
+		model.addAttribute ("name", currentTrack.get (0).getName ());
+		model.addAttribute ("prefix", "/track");
+		model.addAttribute ("courses", currentTrack);
+
+		return "training";
+	}
+}
